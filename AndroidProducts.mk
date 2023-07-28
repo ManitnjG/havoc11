744159cbb3f95b8f5b546b2630885f97694714ab
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2023-07-28 23:14:49 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2020 Havoc-OS
+# Copyright (C) 2020 corvus-OS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_X01BD.mk
+    $(LOCAL_DIR)/corvus_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_X01BD-user \
-    havoc_X01BD-userdebug
+    corvus_X01BD-user \
+    corvus_X01BD-userdebug
